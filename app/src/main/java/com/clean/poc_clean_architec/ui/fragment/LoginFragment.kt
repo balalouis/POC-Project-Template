f60c9bb1c2b870985c5e5443144acb0425355d02
SBALAMU1 <balan.ssn@gmail.com>
2022-08-04 22:24:45 +0530

    UI, Navigation

---
+++ app/src/main/java/com/clean/poc_clean_architec/ui/fragment/LoginFragment.kt
@@ -0,0 +1,33 @@
+package com.clean.poc_clean_architec.ui.fragment
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.clean.poc_clean_architec.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
